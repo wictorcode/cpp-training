98e5d1d4a003dd127fa41fa816174ad445716b50
wictorcode <wictor.dev.pro@gmail.com>
2025-02-03 13:11:02 +0100

    Update README

--- projects/employee_manager/src/main.cpp
+++ projects/employee_manager/src/main.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
 #include "employee.h"
+#include <fstream>
+#include <windows.h>
+
 int main() {
     std::cout << "Employee Management" << std::endl;
     Employee myEmployee;
     myEmployee.setSalary(1000);
     myEmployee.increaseSalary(5.77f);
     std::cout << myEmployee.getSalary() << std::endl;
+
     return 0;
 }
